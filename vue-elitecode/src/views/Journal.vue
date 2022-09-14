8ed96f95283d74094d7e8d24af49066817492417
Shayon Ghoshroy <turing@Turing.lan>
2022-09-14 18:09:20 -0500

    added entry array ref to store variable entries in the Journal page

--- vue-elitecode/src/views/Journal.vue
+++ vue-elitecode/src/views/Journal.vue
@@ -15,17 +15,17 @@
         </div>
 
         <div 
-        v-for="i in 3"
+        v-for="entry in entries"
         class="card mb-2"
         >
             <div class="card-content">
                 <div class="content">
                     <div class="columns is-mobile is-vcentered">
                         <div class="column">
-                            Two pointer.
+                            {{ entry.content }}
                         </div>
                         <div class="column is-5 has-text-right">
-                            <button class="button is-success">
+                            <button class="button is-light">
                                 &check;
                             </button>
                             <button class="button is-danger ml-2">
@@ -40,6 +40,24 @@
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+// todos
+const entries = ref([
+    {
+        id: 1,
+        content: "Two pointer",
+        isDone: false
+    },
+    {
+        id: 2,
+        content: "Sliding Window",
+        isDone: false,
+    },
+]);
+</script>
 
 <style>
     .journal {
