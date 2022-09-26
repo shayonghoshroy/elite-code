a62f7e6e3650d4f21f9b1f94904fa20d323aaa23
Shayon Ghoshroy <turing@Turing.lan>
2022-09-25 22:40:00 -0500

    styled the homepage

--- vue-elitecode/src/views/Home.vue
+++ vue-elitecode/src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
+
+    
 <div class="black" v-for="(week, index) in schedule" :key="week.name">
-    <h3 class="title mb-2">Week {{index + 1}}</h3>
+    <h1 class="title mb-2 has-text-info">Week {{index + 1}}</h1>
     <div v-for="(key, value) in week" :key="key.name">
-        <p class="mt-4">{{value}}</p>
-        <!-- bullet list -->
-        <div v-for="problem in key" :key="problem.name">
-            <li class="content ml-4">{{Object.keys(problem)[0]}}: {{Object.values(problem)[0]}} </li>
-        </div>
+        <div class="content"><h4 class="has-text-white ml-6 mt-5">{{value}}</h4></div>
+                <!-- bullet list -->
+                <div class="concept-list" v-for="problem in key" :key="problem.name" :value="value">
+                    <!-- nested table with key and value-->
+                    <li :class="Object.values(problem)[0]">{{Object.keys(problem)[0]}}</li>
+                </div> 
     </div>
+
+    <div class="divider"></div>
+    
 </div>
+                
 </template>
 
 <script>
@@ -47,11 +54,22 @@
             sortSchedule(temp) {
                 // sort each week in the schedule to be in order by day
                 // loop through each week in temp
+                var sortedDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
                 for (var i = 0; i < temp.length; i++) {
                     // loop through each day in the week
-                    console.log(temp[i])
+                    //console.log(temp[i])
+                    var sortedWeek = temp[i]
+                    // sort the week dict so that the day keys are in order
+                    sortedWeek = Object.keys(sortedWeek).sort(function(a,b) {
+                        return sortedDays.indexOf(a) - sortedDays.indexOf(b);
+                    }).reduce(function (result, key) {
+                        result[key] = temp[i][key];
+                        return result;
+                    }, {});
+                    temp[i] = sortedWeek
                 }
-                //console.log(temp);
+                this.schedule = temp
             },
             async setSchedule() {
                 // get the schedule doc from the schedules collection that has the same uid field as the current user
@@ -82,7 +100,43 @@
             background-color: #181a1b;
             color: #fff;
             padding-top: 30px;
-            text-align: center;
+            padding-left: 5px;
+            /* increase font size */
+            font-size: 1.2em;
+    
         }
+        .divider {
+            border-bottom: 10px solid #fff;
+            width: 50%;
+            margin: auto;
+        }
+
+        .table {
+            margin: auto;
+            background-color: white;
+            color: black;
+            
+        }
+        .easy {
+            color: green
+        }
+        .medium {
+            color: orange
+        }
+        .hard {
+            color: red
+        }
+        .content {
+            color: white;
+        }
+        .concept-list {
+            margin-left: 100px;
+        }
+        .divider {
+            width: 100%;
+            padding-top: 30px;
+            /* change background color */
+        }
+
         
     </style>
